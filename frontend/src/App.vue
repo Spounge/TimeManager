f58311377c2b93a0bcf857c77828fa6238a06f60
Joel Tan <joelv.tan@gmail.com>
2020-10-27 16:25:29 +0100

    [App.vue | Styling]: Global usefull style classes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -26,4 +26,37 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+
+.tile {
+  padding: 10px;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.15);
+
+  .title {
+    padding: 10px;
+    font-size: 20px;
+    font-weight: 600;
+  }
+}
+
+.button-green {
+  padding: 10px 20px;
+  font-size: 18px;
+  font-weight: 600;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  background: #00D46E;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.15);
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+    transform: scale(1.01);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+
 </style>
