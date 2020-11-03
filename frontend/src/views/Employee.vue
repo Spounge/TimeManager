9b90e535e3943dfe61f092086cc58ef281763fe8
Joel Tan <joelv.tan@gmail.com>
2020-11-03 17:05:48 +0100

    [Employee]: Basics layout for employee

--- frontend/src/views/Employee.vue
+++ frontend/src/views/Employee.vue
@@ -1,7 +1,50 @@
 <template>
   <div class="employee">
-    <div class="container">
-      <h1>This is an Employee page</h1>
-    </div>
+    <ClockManager></ClockManager>
+    <WorkingTime></WorkingTime>
+    <WorkingTimes></WorkingTimes>
+    <UserChart></UserChart>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import ClockManager from '../components/ClockManager';
+import WorkingTime from '../components/WorkingTime';
+import WorkingTimes from '../components/WorkingTimes';
+import UserChart from '../components/UserChart';
+
+export default {
+  name: "Home",
+  components: {
+    ClockManager,
+    WorkingTime,
+    WorkingTimes,
+    UserChart,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.employee {
+  display: grid;
+  width: 100%;
+  padding: 20px;
+  gap: 20px;
+  grid-template-rows: repeat(12, 1fr);
+  grid-template-columns: repeat(12, 1fr);
+  grid-template-areas:
+    "A A A A A C C C C C C C"
+    "A A A A A C C C C C C C"
+    "A A A A A C C C C C C C"
+    "B B B B B C C C C C C C"
+    "B B B B B C C C C C C C"
+    "B B B B B C C C C C C C"
+    "D D D D D D D D D D D D"
+    "D D D D D D D D D D D D"
+    "D D D D D D D D D D D D"
+    "D D D D D D D D D D D D"
+    "D D D D D D D D D D D D"
+    "D D D D D D D D D D D D";
+}
+</style>
