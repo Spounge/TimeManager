7a5d12adf732c29fd69d1b2ff7448514e04ad288
Claire7514 <dub.claire@icloud.com>
2020-11-04 11:13:18 +0100

    [settings] : create profile

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -3,13 +3,35 @@
     <header class="header">
         <h1 class="title">Profile information</h1>
     </header>
-    <form>
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname" value="John"><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname" value="Doe"><br><br>
-        <input type="submit" value="Submit">
-    </form>
+    <div class="container">
+      <div class="avatar-circle">
+          <h2>JD</h2>
+        </div>
+      <form class="profile-info">
+        <label class="fname">Username :</label>
+        <v-text-field
+          :rules="rules"
+          hide-details="auto"
+          value="John Doe"
+        ></v-text-field>
+        <br>
+        <label class="fname">Email :</label>
+        <v-text-field
+          :rules="rules"
+          hide-details="auto"
+          value="john.doe@mail.com"
+        ></v-text-field>
+        <br>
+        <label class="fname">Password :</label>
+        <v-text-field
+          :rules="rules"
+          hide-details="auto"
+          type="password"
+          value=""
+        ></v-text-field>
+        <br>
+      </form>
+    </div>
     <footer class="footer">
         <button class="button-blue">Update profile</button>
     </footer>
@@ -24,5 +46,45 @@
 .profile {
     grid-area: F;
 
+    .container {
+      display: flex;
+      justify-content: flex-start;
+      align-items: flex-start;
+
+      margin: 50px;
+    }
+
+    .profile-info {
+      display: flex;
+      justify-content: space-around;
+      flex-direction: column;
+      align-items: flex-start;
+
+      margin-left: 100px;
+
+
+      .fname{
+        font-weight: bold;
+        margin-top: 30px;
+        margin-top: 50px;
+      }
+    }
+
+  .avatar-circle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 175px;
+    height: 175px;
+    border-radius: 100%;
+    font-size: 48px;
+    background: white;
+    border: 5px solid lightgrey;
+    color: lightgrey;
+
+    margin-bottom: 20px;
+  }
 }
+
 </style>
