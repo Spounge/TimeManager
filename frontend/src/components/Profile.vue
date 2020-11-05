ae286e888dd46bdee233417b07c79c87cc2ce123
Joel Tan <joelv.tan@gmail.com>
2020-11-05 16:06:16 +0100

    [Profile]: minor fix of console warning/errors

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="profile tile">
     <header class="header">
-        <h1 class="title">Profile information</h1>
+      <h1 class="title">Profile information</h1>
     </header>
     <div class="container">
       <div class="avatar-circle">
-          <h2>JD</h2>
-        </div>
+        <h2>JD</h2>
+      </div>
       <form class="profile-info">
         <label class="fname">Username :</label>
         <v-text-field
@@ -14,14 +14,14 @@
           hide-details="auto"
           value="John Doe"
         ></v-text-field>
-        <br>
+        <br />
         <label class="fname">Email :</label>
         <v-text-field
           :rules="rules"
           hide-details="auto"
           value="john.doe@mail.com"
         ></v-text-field>
-        <br>
+        <br />
         <label class="fname">Password :</label>
         <v-text-field
           :rules="rules"
@@ -29,46 +29,52 @@
           type="password"
           value=""
         ></v-text-field>
-        <br>
+        <br />
       </form>
     </div>
     <footer class="footer">
-        <button class="button-blue">Update profile</button>
+      <button class="button-blue">Update profile</button>
     </footer>
   </div>
 </template>
 
 <script>
-  
+export default {
+  data: () => ({
+    rules: [
+      (value) => !!value || "Required.",
+      (value) => (value && value.length >= 3) || "Min 3 characters",
+    ],
+  }),
+};
 </script>
 
 <style lang="scss" scoped>
 .profile {
-    grid-area: F;
+  grid-area: F;
 
-    .container {
-      display: flex;
-      justify-content: flex-start;
-      align-items: flex-start;
+  .container {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
 
-      margin: 50px;
+    margin: 50px;
+  }
+
+  .profile-info {
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    align-items: flex-start;
+
+    margin-left: 100px;
+
+    .fname {
+      font-weight: bold;
+      margin-top: 30px;
+      margin-top: 50px;
     }
-
-    .profile-info {
-      display: flex;
-      justify-content: space-around;
-      flex-direction: column;
-      align-items: flex-start;
-
-      margin-left: 100px;
-
-
-      .fname{
-        font-weight: bold;
-        margin-top: 30px;
-        margin-top: 50px;
-      }
-    }
+  }
 
   .avatar-circle {
     display: flex;
@@ -86,5 +92,4 @@
     margin-bottom: 20px;
   }
 }
-
 </style>
